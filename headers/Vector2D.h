3547d918589c9180597800f89c1dfb58e5f11564
SDavidson1177 <sdavidson1177@gmail.com>
2017-07-20 22:17:55 -0400

    Update Vector2D.h

--- headers/Vector2D.h
+++ headers/Vector2D.h
@@ -2,6 +2,16 @@ struct vector2D{
     float x;
     float y;
 
+	vector2D::vector2D() { //added this as a default constructo)
+		x = 0.0;
+		y = 0.0;
+	}
+
+	void setVector2D(float newXValue, float newYValue) { //added this because it is handy and we will probably need it.
+		x = newXValue;
+		y = newYValue;
+	}
+
     vector2D operator+(vector2D& vector2){
         vector2D result;
         result.x = x + vector2.x;
